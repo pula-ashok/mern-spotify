0734b436c27f3766d78f28eaea717332ea39bf49
pula-ashok <ashokpula0307@gmail.com>
2024-11-17 23:27:58 +0530

    completed the bgcolor functionality and arrows in display component

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
 
 const Display = () => {
+    const displayRef = useRef()
+    const locaton = useLocation()
+    const isAlbum = locaton.pathname.includes('album')
+    const albumId = isAlbum && locaton.pathname.slice(-1)
+    const bgColor = albumsData[Number(albumId)].bgColor
+    console.log(bgColor)
+    useEffect(() => {
+        if (isAlbum) {
+            displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`
+        }
+        else {
+            displayRef.current.style.background = `#121212`
+        }
+    })
     return (
-        <div className='w-[100%] m-2 px-6 pt-4 rounded text-white overflow-auto lg:w-[75%] lg:ml-0 bg-[#121212]'>
+        <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded text-white overflow-auto lg:w-[75%] lg:ml-0 bg-[#121212]'>
             <Routes>
                 <Route path='/' element={<DisplayHome />} />
                 <Route path='/album/:id' element={<DisplayAlbum />} />
