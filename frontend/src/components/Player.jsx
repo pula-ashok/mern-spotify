c887ec48aa260b3f73fda4f8868af44e1055885c
pula-ashok <ashokpula0307@gmail.com>
2024-11-18 11:11:03 +0530

    showing currenttime and totaltime functionality completed

--- frontend/src/components/Player.jsx
+++ frontend/src/components/Player.jsx
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react'
-import { assets, songsData } from '../assets/assets'
+import { assets } from '../assets/assets'
 import { PlayerContext } from '../context/PlayerContext'
 
 const Player = () => {
-    const { seekBg, seekBar, play, pause, playStatus } = useContext(PlayerContext)
+    const { seekBg, seekBar, play, pause, playStatus, track, time } = useContext(PlayerContext)
     return (
         <div className='h-[10%] bg-black flex justify-between items-center text-white px-4  overflow-x-hidden'>
             <div className='hidden lg:flex items-center gap-4'>
-                <img src={songsData[0].image} alt="song image" className='w-12' />
+                <img src={track.image} alt="song image" className='w-12' />
                 <div >
-                    <p>{songsData[0].name}</p>
-                    <p>{songsData[0].desc.slice(0, 12)}</p>
+                    <p>{track.name}</p>
+                    <p>{track.desc.slice(0, 12)}</p>
                 </div>
             </div>
 
@@ -26,11 +26,11 @@ const Player = () => {
                     <img src={assets.loop_icon} alt="loop icon" className='w-4 cursor-pointer' />
                 </div>
                 <div className='flex items-center gap-5'>
-                    <p>1.06</p>
+                    <p>{time.currentTime.minute}:{time.currentTime.second}</p>
                     <div ref={seekBg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-                        <hr ref={seekBar} className='h-1 border-none bg-green-800 rounded-full' />
+                        <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full' />
                     </div>
-                    <p>3.20</p>
+                    <p>{time.totalTime.minute}:{time.totalTime.second}</p>
                 </div>
             </div>
             <div className='hidden lg:flex items-center gap-2 opacity-75'>
